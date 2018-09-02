dbb9fbe13c10efa6442910ea1780be75ba5fe6cd
Ignacio Garcia Villanueva <ignacio.gvillanueva@gmail.com>
2018-09-02 22:17:30 +1000

    Fixed typo

--- mindwave.js
+++ mindwave.js
@@ -1,17 +1,17 @@
-var thinkgear = require('node-thinkger-sockets');
+var thinkgear = require('node-thinkgear-sockets');
 var axios = require('axios');
 
-var client = thinkgear.createClient();
+var client = thinkgear.createClient({port: 3000});
 
 client.on('data',function(data){
 	
 	console.log((new Date).toISOString() + ': ' + JSON.stringify(data));
-	axios.post('localhost:3000/post', JSON.stringify(data))
-             .then((res)=>{
-		console.log(res);
-             }).catch((error)=>{
-              console.log(error);
-             })
+	// axios.post('localhost:3000/post', JSON.stringify(data))
+  //            .then((res)=>{
+	// 	console.log(res);
+  //            }).catch((error)=>{
+  //             console.log(error);
+  //            })
 });
 
 client.connect();
